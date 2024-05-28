1b26cbf69a7212fb723c5e62c7b6f87a0af53773
p-diogo.fonteles <p-diogo.fonteles@pd.tec.br>
2024-05-28 14:08:24 -0300

    feat: add header component

---
+++ src/presentation/components/header/header.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import icon from '@/app/icon.png'
+import './header.css'
+
+export function Header() {
+  return (
+    <header className="header">
+      <div className="header-container">
+        <div className="header-logo">
+          <Image src={icon} alt="Icon" width={38} height={38} />
+          <span>Survival Nexus</span>
+        </div>
+        <nav className="header-nav">
+          <Link href="/report">Report</Link>
+          <Link href="/survivors">Survivors</Link>
+          <Link href="/inventory">Inventory</Link>
+        </nav>
+        <div className="header-profile">
+          <Image
+            src="https://github.com/diogofonteles.png"
+            alt="Profile"
+            width={32}
+            height={32}
+          />
+        </div>
+      </div>
+    </header>
+  )
+}
